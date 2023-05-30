dbc474300330d9d0d9c50605a25177f34433baf5
OanhPV <poanh1002@gmail.com>
2023-05-30 10:42:53 +0700

    Data binding properties

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  public  name = "Oanh";
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 }
